4b21ddf3f22a3f9e8fe3b9e3aec110252e67edee
JavierLazzarino <javier.lazzarino@gmail.com>
2017-04-15 01:31:15 -0300

    Update NumeroNegativo.java

--- AyPII-TP-1/src/exceptions/NumeroNegativo.java
+++ AyPII-TP-1/src/exceptions/NumeroNegativo.java
@@ -2,9 +2,13 @@ package exceptions;
 
 public class NumeroNegativo extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4225559273211977393L;
+
 	public NumeroNegativo(String mensaje) {
 		
 		super(mensaje);
 	}
-
 }
