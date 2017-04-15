05cb5bd176781275a9905a7668680d910aabbe8c
JavierLazzarino <javier.lazzarino@gmail.com>
2017-04-15 01:30:04 -0300

    Update CuilInvalido.java

--- AyPII-TP-1/src/exceptions/CuilInvalido.java
+++ AyPII-TP-1/src/exceptions/CuilInvalido.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class CuilInvalido extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5031208830367135099L;
+
 	public CuilInvalido (String mensaje) {
 		
 		super(mensaje);
