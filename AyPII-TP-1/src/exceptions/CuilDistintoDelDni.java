9fff9479990e31846f2c8cc3b95abe5b47cc07d4
JavierLazzarino <javier.lazzarino@gmail.com>
2017-04-15 01:29:33 -0300

    Update CuilDistintoDelDni.java

--- AyPII-TP-1/src/exceptions/CuilDistintoDelDni.java
+++ AyPII-TP-1/src/exceptions/CuilDistintoDelDni.java
@@ -2,9 +2,13 @@ package exceptions;
 
 public class CuilDistintoDelDni extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2893604326937044806L;
+
 	public CuilDistintoDelDni(String mensaje) {
 		
 		super(mensaje);		
 	}
-
 }
