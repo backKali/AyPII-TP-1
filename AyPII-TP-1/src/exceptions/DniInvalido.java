111b686d0af0bc20c03fe0b8514315174389e8ef
JavierLazzarino <javier.lazzarino@gmail.com>
2017-04-15 01:30:27 -0300

    Update DniInvalido.java

--- AyPII-TP-1/src/exceptions/DniInvalido.java
+++ AyPII-TP-1/src/exceptions/DniInvalido.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class DniInvalido extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5031208830367135099L;
+
 	public DniInvalido (String mensaje) {
 		
 		super(mensaje);
