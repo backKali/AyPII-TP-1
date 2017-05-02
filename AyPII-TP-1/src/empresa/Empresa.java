331cef5d5c9d78fc5c476adea1cc7d3902b3059d
backKali <matias.uno.cualquiera@hotmail.com>
2017-05-02 15:23:15 -0300

    Update Empresa.java

--- AyPII-TP-1/src/empresa/Empresa.java
+++ AyPII-TP-1/src/empresa/Empresa.java
@@ -79,7 +79,6 @@ public class Empresa {
 	 * posee la empresa
 	 */
 
-	// Hay que hacer tests de esto
 	
 	public void fileWriter(String file) throws IOException {
 		//Convirtiendo el Hash a Array antes de iterarlo
